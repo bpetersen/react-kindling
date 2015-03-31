9b52b094fde00b58b52e604227a24dbdd7c659ed
Barton A Petersen <bart.petersen@gmail.com>
2015-03-31 05:54:28 -0600

    removing png from url-loader in webpack.config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -76,7 +76,7 @@ module.exports = function(release){
         //{ test: /\.html$/,            loader: 'webpack-compile-templates' }, // Add if you need to compile underscore.js - https://www.npmjs.com/package/webpack-compile-templates
         //{ test: /.*\.(gif|png|jpg|jpeg|svg)$/, loaders: ['file?hash=sha512&digest=hex&size=16&name=[hash].[ext]', 'image-webpack-loader?optimizationLevel=7&interlaced=false']},
         //{ test: /.*\.(eot|woff2|woff|ttf)/,    loader: 'file?hash=sha512&digest=hex&size=16&name=cd [hash].[ext]'}
-        { test: /\.(png|woff|woff2|eot|ttf|svg)($|\?)/, loader: 'url-loader' }
+        { test: /\.(woff|woff2|eot|ttf|svg)($|\?)/, loader: 'url-loader' }
       ]
       
     }
